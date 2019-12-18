f8d16f229d8fae0cfc71631b06efd8808e674cf9
asmirnov69 <asmirnov69@users.noreply.github.com>
2019-12-18 00:00:50 +0000

    added kinda COmmunicator and manually written proxy

--- examples/simplest.dpl/frontend/App.js
+++ examples/simplest.dpl/frontend/App.js
@@ -2,27 +2,51 @@ import React from 'react';
 
 let c = 0;
 
+class Communicator {
+    constructor(socket) {
+	this.socket = socket;
+	this.response_handler = null;
+	this.socket.on('remote-call-response', (res) => this.response_handler(res));
+    }
+    
+    do_call(args) {
+	return new Promise((resolve, reject) => {	    
+	    this.response_handler = (res) => {
+		resolve(res);
+	    };
+	    this.socket.emit('remote-call', args);
+	});
+    };
+}
+
+class Hello {
+    constructor(proxy, remote_obj_id) {
+	this.proxy = proxy;
+	this.remote_obj_id = remote_obj_id;
+    }
+
+    sayHello() {
+	let args = {'obj_id': this.remote_obj_id,
+		    'call_method': 'sayHello',
+		    'args': {}};
+	return this.proxy.do_call(args);
+    }
+}
+
 class App extends React.Component {
     constructor(props) {
 	super(props);
 	this.state = {greeting: 'none'};
 	this.onClick = this.onClick.bind(this);
-    }
-
-    setSocketListeners () {
-	this.props.socket.on('remote-call-response', (res) => {
-	    console.log("received", res);
-	    this.setState({greeting: res.ret});
-	});
-    }
-    
-    componentDidMount() {
-	this.setSocketListeners();
+	this.communicator = new Communicator(this.props.socket);
+	this.hello_prx = new Hello(this.communicator, 'Hello');
     }
 
     onClick() {
-	//debugger;
-	this.props.socket.emit('remote-call', {'obj_id': 'Hello', 'call_method': 'sayHello', 'args': {}});
+	this.hello_prx.sayHello().then((res) => {
+	    console.log("received", res);
+	    this.setState({greeting: res.ret});
+	});
     }
     
     render() {
