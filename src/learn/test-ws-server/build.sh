3c85df888865f6a86a4db6715e638ac51f27ea30
Andrei Smirnov <andrei.smirnov.69@gmail.com>
2020-02-15 21:55:15 +0000

    learn moved to to learn repo

--- src/learn/test-ws-server/build.sh
+++
@@ -1,6 +0,0 @@
-# uSockets
-gcc -c -fPIC -DLIBUS_NO_SSL -I$HOME/uWebSockets/uSockets/src $HOME/uWebSockets/uSockets/src/*.c
-gcc -c -fPIC -DLIBUS_NO_SSL -I$HOME/uWebSockets/uSockets/src $HOME/uWebSockets/uSockets/src/eventing/*.c
-gcc -c -fPIC -DLIBUS_NO_SSL -I$HOME/uWebSockets/uSockets/src $HOME/uWebSockets/uSockets/src/crypto/*.c
-
-g++ -g -flto -lpthread -std=c++17 -I$HOME/uWebSockets/src -I$HOME/uWebSockets/uSockets/src MyEchoServer.cpp -o MyEchoServer *.o -lz
