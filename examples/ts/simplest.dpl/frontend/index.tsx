976763d7227bbe147632aaa49a86e5c9e8729e0d
Andrei Smirnov <andrei.smirnov.69@gmail.com>
2020-02-16 00:31:39 +0000

    minor

--- examples/ts/simplest.dpl/frontend/index.tsx
+++ examples/ts/simplest.dpl/frontend/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 //import {getBackendCommunicator} from 'libdipole-js';
 import {getBackendCommunicator, Communicator} from './Communicator';
-import App from './App.js';
+import App from './App';
 
 getBackendCommunicator().then((communicator: Communicator) => {
     ReactDOM.render(<App communicator={communicator}/>, document.getElementById('root'));
