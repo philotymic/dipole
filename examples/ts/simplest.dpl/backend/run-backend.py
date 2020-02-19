66a3cbf7d142ab4b2f9ac5a98e9a32058a906a3e
Andrei Smirnov <andrei.smirnov.69@gmail.com>
2020-02-19 17:53:28 +0000

    ts simplest example works -- still no libdipole-ts

--- examples/ts/simplest.dpl/backend/run-backend.py
+++ examples/ts/simplest.dpl/backend/run-backend.py
@@ -1,18 +1,19 @@
 #import ipdb
-import sys, os
+import sys, os, asyncio
 import prctl, signal
-import time
 
+import websockets
 sys.path.append(os.path.join(os.environ['dipole_topdir'], "src"))
 import libdipole
+import libdipole.autoport
 
 @libdipole.exportclass
 class Hello:
-    def sayHello(self) -> str:
+    def sayHello(self):
         print("Hello World!")
         return "Hello, World!"
 
-    def sayAloha(self, language: str) -> str:
+    def sayAloha(self, language):
         print("Aloha")
         #time.sleep(3)
         return language + "Aloha"
@@ -22,14 +23,17 @@ if __name__ == "__main__":
         # https://github.com/seveas/python-prctl -- prctl wrapper module
         # more on pdeathsignal: https://stackoverflow.com/questions/284325/how-to-make-child-process-die-after-parent-exits
         prctl.set_pdeathsig(signal.SIGTERM) # if parent dies this child will get SIGTERM
-
-    dpl_server = libdipole.mod_libdipole.DipoleServer()
-    dpl_event_handler = libdipole.BackendEventHandler(libdipole.port_assignment_handler, sys.argv[1])
-    dpl_server.set_event_handler(dpl_event_handler)
-    dispatcher = libdipole.Dispatcher(dpl_server)
-    dpl_event_handler.dispatcher = dispatcher
+    xfn = sys.argv[1]
     
+    dispatcher = libdipole.Dispatcher()
+    dpl_server = libdipole.DipoleServer(dispatcher)
+
     print("adding object Hello")
     dispatcher.add_object("Hello", Hello())
 
-    dpl_server.run_listener(0)
+    dpl_server_l = websockets.serve(dpl_server.message_loop, 'localhost', 0)
+    asyncio.get_event_loop().run_until_complete(dpl_server_l)
+    assigned_port = libdipole.autoport.find_ws_port(dpl_server_l)
+    print("assigned_port:", assigned_port)
+    libdipole.port_assignment_handler(assigned_port, xfn)
+    asyncio.get_event_loop().run_forever()
